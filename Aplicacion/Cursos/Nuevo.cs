8e92e58d5c72a43da34afb59c41578b3942b55f7
Isaias Cordova <isaias.cordova@licenciaapi.com>
2024-09-20 11:18:56 -0600

    Modificaciones en NuevoCS para la insercion de cursos

--- Aplicacion/Cursos/Nuevo.cs
+++ Aplicacion/Cursos/Nuevo.cs
@@ -19,6 +19,8 @@ namespace Aplicacion.Cursos
             public string Titulo { get; set; }
             public string Descripcion { get; set; }
             public DateTime? FechaPublicacion { get; set; }
+
+            public List<Guid> ListaInstructor { get; set; }
         }
 
         public class EjecutaValidacion : AbstractValidator<Ejecuta>
@@ -41,20 +43,37 @@ namespace Aplicacion.Cursos
 
             public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
             {
+                Guid _cursoId = Guid.NewGuid();
                 var curso = new Curso
                 {
+                    CursoId = _cursoId,
                     Titulo = request.Titulo,
                     Descripcion = request.Descripcion,
                     FechaPublicacion = request.FechaPublicacion ?? DateTime.Now,
                 };
 
                 _context.Curso.Add(curso);
-                var valor = await _context.SaveChangesAsync();
-                if(valor > 0)
+
+                if (request.ListaInstructor != null)
                 {
-                    return Unit.Value;
+                    foreach (var id in request.ListaInstructor)
+                    {
+                        var cursoInstructor = new CursoInstructor
+                        {
+                            CursoId = _cursoId,
+                            InstructorId = id,
+                        };
+                        _context.CursoInstructor.Add(cursoInstructor);
+                    }
                 }
-                throw new Exception("No se pudo insertar el curso");
+                {
+                    var valor = await _context.SaveChangesAsync();
+                    if (valor > 0)
+                    {
+                        return Unit.Value;
+                    }
+                    throw new Exception("No se pudo insertar el curso");
+                }
             }
         }
     }
